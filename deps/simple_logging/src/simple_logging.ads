7b6077df69555f56d1774e1949c691a9063e523e
Alejandro R. Mosteo <alejandro@mosteo.com>
2018-02-07 01:04:53 +0100

    Simple logging lib

---
+++ deps/simple_logging/src/simple_logging.ads
@@ -0,0 +1,16 @@
+package Simple_Logging with Preelaborate is
+
+   type Levels is (Error,
+                   Warning,
+                   Info,
+                   Detail,
+                   Debug);
+   --  From most important to less important
+
+   Level : Levels := Info;
+   --  Any message at the same level or below will be output to console
+
+   procedure Log (S : String; Level : Levels := Info);
+   --  Report a log message
+
+end Simple_Logging;
