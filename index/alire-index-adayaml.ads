3e795d908d74060b8898099104611076eb05ae0f
Alejandro R. Mosteo <jano@miles>
2018-03-23 17:02:31 +0100

    AdaYaml 0.3

--- index/alire-index-adayaml.ads
+++ index/alire-index-adayaml.ads
@@ -1,4 +1,5 @@
 with Alire.Index.AUnit;
+with Alire.Index.DAK_Components;
 
 package Alire.Index.AdaYaml is
 
@@ -7,6 +8,34 @@ package Alire.Index.AdaYaml is
    Prj_Repo       : constant URL    := "https://github.com/yaml/AdaYaml.git";
    Prj_Author     : constant String := "Felix Krause";
    Prj_Website    : constant URL    := "https://ada.yaml.io/";
+
+   V_0_3 : constant Release :=
+             Project.Register
+               (V ("0.3"),
+                Git (Prj_Repo, "2017a7c2523499c03b8d7fe06546a5a8bae6476d"),
+
+                Dependencies       =>
+                  AUnit.Project >= AUnit.V_2017 and
+                  DAK_Components.Components_Connections_V_4_27.Within_Major,
+
+                Properties         =>
+                  Project_File ("yaml.gpr") and
+                  Project_File ("yaml-utils.gpr") and
+                  Project_File ("yaml-annotation_processor.gpr") and
+                  Project_File ("yaml-server.gpr") and
+
+                  GPR_Scenario ("Mode", "debug" or "release") and
+
+                  Author     (Prj_Author) and
+                  Website    (Prj_Website) and
+                  License    (MIT),
+
+                Private_Properties =>
+                  Project_File ("yaml-tests.gpr") and
+
+                  Executable ("yaml-lexer-harness") and
+                  Executable ("yaml-parser-harness")
+               );
 
    V_0_2 : constant Release :=
                Project.Register
