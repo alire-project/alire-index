00d101fff7aad80babda11bb4d04970b215845cf
Alejandro R Mosteo <alejandro@mosteo.com>
2018-06-05 17:01:06 +0200

    ncursesada from debian testing

--- index/native/alire-index-ncursesada.ads
+++ index/native/alire-index-ncursesada.ads
@@ -5,24 +5,28 @@ package Alire.Index.NcursesAda is
    function Project is
      new Catalogued_Project ("Ada binding to the ncurses text interface library");
 
-   V_6 : constant Release :=
-           Project.Register
-             (V ("6"),
-              Native ((Debian | Ubuntu => Packaged_As ("libncursesada5-dev"),
-                       others          => Unavailable)),
+   Base : constant Release := Project.Unreleased
+     (Dependencies => GNAT.Project.Current);
 
-              Dependencies =>
-                GNAT.Project.Current
-             );
+   package V_6_1_20180127 is new Project_Release
+     (Base.Replacing
+        (Origin =>
+              Native ((Debian |
+                       Ubuntu => Packaged_As ("libncursesada6.1.20180127-dev"),
+                       others => Unavailable))));
 
-   V_5 : constant Release :=
-           Project.Register
-             (V ("5"),
-              Native ((Debian | Ubuntu => Packaged_As ("libncursesada3-dev"),
-                       others          => Unavailable)),
+   package V_6 is new Project_Release
+     (Base.Replacing
+        (Origin =>
+              Native ((Debian |
+                       Ubuntu => Packaged_As ("libncursesada5-dev"),
+                       others => Unavailable))));
 
-              Dependencies =>
-                GNAT.Project.Current
-             );
+   package V_5 is new Project_Release
+     (Base.Replacing
+        (Origin =>
+              Native ((Debian |
+                       Ubuntu => Packaged_As ("libncursesada3-dev"),
+                       others => Unavailable))));
 
 end Alire.Index.NcursesAda;
